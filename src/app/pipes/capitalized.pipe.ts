046d7032766a4d61731d598c8c2dea54980739fc
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-01 01:49:01 -0500

    logic to change to first letter for every word capitalized or first letter only

--- src/app/pipes/capitalized.pipe.ts
+++ src/app/pipes/capitalized.pipe.ts
@@ -5,8 +5,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizedPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  // Los args son los argumentos que le mandamos al pipe desde la vista (miNombre:1:true) por ejemplo
+  transform(value: string, todas: boolean = true): string {
+    //console.log(value);
+    //console.log(args);
+
+    value = value.toLocaleLowerCase();
+
+    // Separamos los elementos del arreglo o del elemento por espacios vacíos ('')
+    let nombres = value.split(' ');
+
+    if( todas ){
+      // El método map crea un nuevo arreglo con los resultados de la llamada a la función indicada aplicada a cada uno de sus elementos.                           Luego en el return pasamos la primera letra a mayúsculas y le concatenamos lo que resta de la palabra.                                              Con el join unimos los elementos del arreglo en una cadena y devolvemos esa cadena separada por un espacio en este caso
+      nombres = nombres.map(nombre =>{
+        return nombre[0].toUpperCase() + nombre.substr(1); // Juan Manuel López Santana
+      })
+      console.log(nombres);
+      return nombres.join(' ');
+    }else{
+      // Juan manuel lópez santana; los nombres[0] es la primera palabra, los nombres[0][1] es la primera palabra en la primera posición 
+      nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substr(1);
+    }
+    return nombres.join(' ');
   }
 
 }
